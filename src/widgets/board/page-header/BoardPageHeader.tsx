fd62ef96b2389892e33d440ad3fee4f01ab1b431
nokkup <kuprin.kirill.96@gmail.com>
2023-07-06 16:35:18 +0300

    feat: add board page-header component

---
+++ src/widgets/board/page-header/BoardPageHeader.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './BoardPageHeader.module.scss';
+import { Heading, AddTaskIcon } from '@/shared/ui-kit';
+
+export const BoardPageHeader = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <Heading size='lg'>{t('board.title')}</Heading>
+      <AddTaskIcon onClick={() => navigate('/task/create')} />
+    </div>
+  );
+};
---
+++ src/widgets/board/page-header/index.ts
@@ -0,0 +1,1 @@
+export { BoardPageHeader } from './BoardPageHeader';
