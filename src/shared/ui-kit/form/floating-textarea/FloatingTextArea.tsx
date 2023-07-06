60878e803759a37ca38c1dfd7a37980a767f1013
nokkup <kuprin.kirill.96@gmail.com>
2023-07-06 15:06:02 +0300

    feat: add floating-textarea component

---
+++ src/shared/ui-kit/form/floating-textarea/FloatingTextArea.tsx
@@ -0,0 +1,39 @@
+import { InputHTMLAttributes, PropsWithChildren, forwardRef, useId } from 'react';
+import clsx from 'clsx';
+
+import styles from './FloatingTextArea.module.scss';
+
+export type FloatingTextAreaProps = Omit<InputHTMLAttributes<HTMLTextAreaElement>, 'placeholder'> &
+  PropsWithChildren<{
+    label: string;
+    isInvalid?: boolean;
+    errorMessage?: string;
+  }>;
+
+export const FloatingTextArea = forwardRef<HTMLTextAreaElement, FloatingTextAreaProps>(
+  (props, ref) => {
+    const id = useId();
+    const { label, className, isInvalid, errorMessage, ...textareaProps } = props;
+
+    const classes = clsx(className, styles.floatingTeaxArea, {
+      [styles.floatingTeaxArea_invalid]: isInvalid,
+    });
+
+    return (
+      <div className={classes}>
+        <textarea
+          id={id}
+          ref={ref}
+          aria-invalid={isInvalid}
+          aria-errormessage={errorMessage}
+          className={styles.textarea}
+          placeholder={label}
+          {...textareaProps}
+        />
+        <label className={styles.label} htmlFor={id}>
+          {label}
+        </label>
+      </div>
+    );
+  }
+);
