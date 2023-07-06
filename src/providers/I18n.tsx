8c38c224e40b2335c9c4674bcdab5571e1db8fd8
nokkup <kuprin.kirill.96@gmail.com>
2023-07-06 12:00:32 +0300

    feat: add i18n provider

---
+++ src/providers/I18n.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from 'react';
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+
+i18n.use(Backend).use(initReactI18next).init({
+  lng: 'ru',
+  fallbackLng: 'ru',
+});
+
+export const I18n = (props: PropsWithChildren) => (
+  <I18nextProvider i18n={i18n} defaultNS={'translation'} {...props} />
+);
