31534657d53ad2d638efdd566836f5d6051cfed0
nokkup <kuprin.kirill.96@gmail.com>
2023-07-06 12:24:01 +0300

    feat: add text component

---
+++ src/shared/ui-kit/typography/text/Text.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+import { HTMLAttributes, PropsWithChildren } from 'react';
+
+import styles from './Text.module.scss';
+
+type TextProps = HTMLAttributes<HTMLSpanElement> &
+  PropsWithChildren<{
+    tag: 'p' | 'span';
+    size?: 'sm' | 'md' | 'xsm';
+    weight?: 'bold' | 'light' | 'normal';
+  }>;
+
+export const Text = (props: TextProps) => {
+  const { tag = 'p', size = 'md', weight = 'normal', className, ...otherProps } = props;
+  const TextComponent = tag;
+
+  const classes = clsx(className, styles.text, {
+    [styles.text_md]: size === 'md',
+    [styles.text_sm]: size === 'sm',
+    [styles.text_xsm]: size === 'xsm',
+
+    [styles.text_bold]: weight === 'bold',
+    [styles.text_light]: weight === 'light',
+    [styles.text_normal]: weight === 'normal',
+  });
+
+  return <TextComponent className={classes} {...otherProps} />;
+};
