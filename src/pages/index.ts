aef664049822c92bae9caa89e8db58860ecbc156
nokkup <kuprin.kirill.96@gmail.com>
2023-07-07 13:11:28 +0300

    feat: widgets page

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { AuthPage } from './auth';
+export { BoardPage } from './board';
+export { WidgetsPage } from './widgets';
+export { TaskCreatePage } from './task-create';
+export { TaskEditPage } from './task-edit';
+export { TaskInfoPage } from './task-info';
