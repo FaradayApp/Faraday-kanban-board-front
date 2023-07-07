06706d578654e8b44441f02ca90d37ee94ab80ce
nokkup <kuprin.kirill.96@gmail.com>
2023-07-07 12:33:58 +0300

    feat: change props

--- src/enitities/user/short-card/UserShortCard.tsx
+++ src/enitities/user/short-card/UserShortCard.tsx
@@ -1,21 +1,24 @@
 import { ReactNode } from 'react';
 
 import styles from './UserShortCard.module.scss';
-import { AvatarIcon, Text } from '@/shared/ui-kit';
+import { Avatar, Text } from '@/shared/ui-kit';
+import { usersStore } from '@/stores/Users';
 
 type UserShortCardProps = {
-  name: string;
+  userId: UserId;
   controls?: ReactNode;
 };
 
 export const UserShortCard = (props: UserShortCardProps) => {
-  const { name, controls } = props;
+  const { userId, controls } = props;
+
+  const user = usersStore.getById(userId);
 
   return (
     <div className={styles.userShortCard}>
-      <AvatarIcon width={30} height={30} />
+      <Avatar size={30} src={user.avatar} />
       <Text tag='span' size='sm'>
-        {name}
+        {user.name}
       </Text>
       {controls && <div className={styles.userShortCard__controls}>{controls}</div>}
     </div>
