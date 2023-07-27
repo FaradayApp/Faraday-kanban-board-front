3d437d269b4ac9bea9ee02109331f17f08e5e987
nokkup <kuprin.kirill.96@gmail.com>
2023-07-27 18:14:42 +0300

    feat: add new Task type

--- src/enitities/task/task.types.ts
+++ src/enitities/task/task.types.ts
@@ -1,12 +1,20 @@
-export type TaskStatus = 'BACKLOG' | 'TODO' | 'IN_PROGRESS' | 'DONE' | 'ARCHIVE';
-export type TaskPriority = 'MEDIUM' | 'LOW' | 'HIGH';
+import { Dayjs } from 'dayjs';
+
+export type TaskStatus = 'BACKLOG' | 'TODO' | 'IN_PROGRESS' | 'DONE' | 'ARCHIVE' | '';
+export type TaskPriority = 'MEDIUM' | 'LOW' | 'HIGH' | '';
 
 export type Task = {
   id: number;
   title: string;
-  expiration_date: string;
-  status: TaskStatus;
-  priority: TaskPriority;
+  expiration_date: Dayjs;
+  status: {
+    type: TaskStatus;
+    weight: number;
+  };
+  priority: {
+    type: TaskPriority;
+    weight: number;
+  };
   // backend will be changed
   performers: unknown[];
 };
