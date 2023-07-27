8fe8402f8f5cffef6558379632603debeba1c06f
nokkup <kuprin.kirill.96@gmail.com>
2023-07-27 18:15:47 +0300

    feat: add task entity public api

--- src/enitities/task/index.ts
+++ src/enitities/task/index.ts
@@ -1,1 +1,3 @@
 export { type TaskStatus, type TaskPriority, type Task } from './task.types';
+export { TaskStatusSelect } from './TaskStatusSelect';
+export { TaskPrioritySelect } from './TaskPrioritySelect';
