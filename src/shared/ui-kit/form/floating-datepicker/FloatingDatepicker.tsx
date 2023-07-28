a7d18332ed50190469830458958f45e546f12013
nokkup <kuprin.kirill.96@gmail.com>
2023-07-28 16:28:13 +0300

    fix: datepicker error style

--- src/shared/ui-kit/form/floating-datepicker/FloatingDatepicker.tsx
+++ src/shared/ui-kit/form/floating-datepicker/FloatingDatepicker.tsx
@@ -9,16 +9,17 @@ type FloatingDetepickerProps = {
   label: string;
   value: Dayjs;
   onChange: (newValue: Dayjs) => void;
+  isInvalid?: boolean;
   errorMessage?: string;
 };
 
 export const FloatingDetepicker = forwardRef<HTMLInputElement, FloatingDetepickerProps>(
   (props, ref) => {
     const id = useId();
-    const { label, errorMessage, value, onChange } = props;
+    const { label, value, errorMessage, isInvalid, onChange } = props;
 
     const containerClasses = clsx(styles.floatingDatepicker, {
-      [styles.floatingDatepicker_invalid]: errorMessage,
+      [styles.floatingDatepicker_invalid]: isInvalid || errorMessage,
       [styles.floatingDatepicker_unfilled]: !value,
       [styles.floatingDatepicker_filled]: !!value,
     });
@@ -41,6 +42,7 @@ export const FloatingDetepicker = forwardRef<HTMLInputElement, FloatingDetepicke
           ref={ref}
           placeholder={label}
           className={styles.datepicker}
+          aria-invalid={!!errorMessage || isInvalid}
           aria-errormessage={errorMessage}
           onChange={onDateChange}
         />
