a384ca31e72c63c6e7147a65c596694dc31bd052
nokkup <kuprin.kirill.96@gmail.com>
2023-07-28 18:58:14 +0300

    feat: auth errors

--- src/shared/errors/auth-errors.ts
+++ src/shared/errors/auth-errors.ts
@@ -1,4 +1,4 @@
 export class AuthError extends Error {}
 export class WrongCredentialsError extends AuthError {}
-export class WrongResponseError extends AuthError {}
 export class RefreshTokenError extends AuthError {}
+export class AccessDeniedError extends AuthError {}
