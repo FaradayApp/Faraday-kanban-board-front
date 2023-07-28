be08d1aed8bdb45f5b0a688007cd753548d7f929
nokkup <kuprin.kirill.96@gmail.com>
2023-07-28 13:17:54 +0300

    wip: TaskPriority type

--- src/widgets/board/task-card/TaskCard.tsx
+++ src/widgets/board/task-card/TaskCard.tsx
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs';
 import { observer } from 'mobx-react-lite';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -13,58 +12,54 @@ import {
   PriorityTag,
   Avatar,
 } from '@/shared/ui-kit';
-import { usersStore, Task } from '@/stores';
+import { Task } from '@/enitities/task';
 
-type TaskCardProps = {
-  task: Task;
-};
-
-export const TaskCard = observer(({ task }: TaskCardProps) => {
+export const TaskCard = observer((props: Task) => {
+  const { id, title, priority, performers, expiration_date } = props;
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const selectedUsers = task.data.workers?.slice(0, 3) ?? [];
-  const formattedDate = dayjs(task.data.dates.end, { format: 'DD.MM.YYYY' }).format('DD MMM');
+  const users = performers?.slice(0, 3) ?? [];
+  const formattedDate = expiration_date.format('DD MMM');
+  const comments = 0;
 
   const openTaskInfo = () => {
-    navigate(`/task/${task.data.id}`);
+    navigate(`/task/${id}`);
   };
-
-  const Users = selectedUsers.map((id, i) => (
-    <div key={id} style={{ position: 'absolute', left: `${14 * i}px` }}>
-      <Avatar size={20} src={usersStore.getById(id).avatar} />
-    </div>
-  ));
-
-  const Comments = task.data.comments ? (
-    <div className={styles.taskCard__controls}>
-      <MenuIcon />
-      <div className={styles.taskCard__controlsComments}>
-        <Text tag='span' size='xsm'>
-          {task.data.comments.length}
-        </Text>
-        <CommentIcon />
-      </div>
-    </div>
-  ) : null;
 
   return (
     <article className={styles.taskCard} onClick={openTaskInfo}>
       <Heading tag='h3' size='sm' weight='bold' className={styles.taskCard__title}>
-        {task.data.name}
+        {title}
       </Heading>
       <div className={styles.taskCard__content}>
         <div className={styles.taskCard__priority}>
           <Text tag='span' size='xsm' weight='light'>
             {t('task.priority.label')}
           </Text>
-          <PriorityTag type={task.data.priority} />
+          <PriorityTag type={priority.type} />
         </div>
         <DateTag date={formattedDate} />
       </div>
       <footer className={styles.taskCard__footer}>
-        <div className={styles.taskCard__workers}>{Users}</div>
-        {Comments}
+        <div className={styles.taskCard__workers}>
+          {users.map((id, i) => (
+            <div key={id as string} style={{ position: 'absolute', left: `${14 * i}px` }}>
+              <Avatar size={20} src={''} />
+            </div>
+          ))}
+        </div>
+        {!!comments && (
+          <div className={styles.taskCard__controls}>
+            <MenuIcon />
+            <div className={styles.taskCard__controlsComments}>
+              <Text tag='span' size='xsm'>
+                {comments}
+              </Text>
+              <CommentIcon />
+            </div>
+          </div>
+        )}
       </footer>
     </article>
   );
