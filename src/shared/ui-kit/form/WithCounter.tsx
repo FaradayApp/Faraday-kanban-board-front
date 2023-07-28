f5f90dd67b7559f45a11ef0c058074d8dbbc5a1f
nokkup <kuprin.kirill.96@gmail.com>
2023-07-28 16:29:27 +0300

    feat: input and textarea with symbols counter

---
+++ src/shared/ui-kit/form/WithCounter.tsx
@@ -0,0 +1,4 @@
+import { FloatingInput, FloatingTextArea, withSymbolsCounter } from '.';
+
+export const InputWithCounter = withSymbolsCounter(FloatingInput);
+export const TextAreaWithCounter = withSymbolsCounter(FloatingTextArea);
--- src/shared/ui-kit/form/index.ts
+++ src/shared/ui-kit/form/index.ts
@@ -4,3 +4,4 @@ export { FloatingInput } from './floating-input';
 export { FloatingTextArea } from './floating-textarea';
 export { FloatingSelect, type SelectOption } from './floating-select';
 export { FloatingDetepicker } from './floating-datepicker';
+export { InputWithCounter, TextAreaWithCounter } from './WithCounter';
