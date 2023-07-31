978dc4be791998bf46edc092b263423b944f71b9
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 13:07:43 +0300

    feat: api public import

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,6 +1,2 @@
 export * from './auth';
-export * from './create-task';
-export * from './get-board';
-export * from './get-tasks';
-export * from './get-users';
-export * from './get-widgets';
+export * from './tasks';
