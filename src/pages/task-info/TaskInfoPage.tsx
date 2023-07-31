f422d1cb2680777818c1d73c09c8377878e2e03c
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 14:59:47 +0300

    feat: load task info

--- src/pages/task-info/TaskInfoPage.tsx
+++ src/pages/task-info/TaskInfoPage.tsx
@@ -1,15 +1,27 @@
+import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { PageContainer } from '@/shared/ui-kit';
+import { taskInfoStore } from '@/stores/task-info';
 import { TaskInfoForm, TaskPageHeader } from '@/widgets/task';
 
 export const TaskInfoPage = observer(() => {
   const { t } = useTranslation();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      taskInfoStore.init(id);
+    }
+  }, [id]);
+
+  const data = taskInfoStore.taskInfo.data || null;
 
   return (
     <PageContainer header={<TaskPageHeader title={t('task.title')} />}>
-      <TaskInfoForm />
+      {data && <TaskInfoForm task={data} />}
     </PageContainer>
   );
 });
