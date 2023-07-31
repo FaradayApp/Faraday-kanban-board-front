d62d7f42235ffe6afc074078d7886b3ac1882c73
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 14:24:33 +0300

    feat: task edit page

--- src/pages/task-edit/TaskEditPage.tsx
+++ src/pages/task-edit/TaskEditPage.tsx
@@ -1,15 +1,39 @@
+import { useCallback, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { PageContainer } from '@/shared/ui-kit';
+import { type TaskInfo } from '@/enitities/task';
+import { boardStore } from '@/stores/board';
+import { taskInfoStore } from '@/stores/task-info';
+import { editTaskInfo } from '@/features/tasks';
 import { TaskEditForm, TaskPageHeader } from '@/widgets/task';
 
 export const TaskEditPage = observer(() => {
   const { t } = useTranslation();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      taskInfoStore.init(id);
+    }
+  }, [id]);
+
+  const editTask = useCallback(
+    (data: Partial<TaskInfo>) => {
+      if (id) {
+        editTaskInfo(boardStore, id)(data);
+      }
+    },
+    [id]
+  );
+
+  const data = taskInfoStore.taskInfo.data || null;
 
   return (
     <PageContainer header={<TaskPageHeader title={t('taskEdit.titles.edit')} />}>
-      <TaskEditForm />
+      {data && <TaskEditForm task={data} editTask={editTask} />}
     </PageContainer>
   );
 });
