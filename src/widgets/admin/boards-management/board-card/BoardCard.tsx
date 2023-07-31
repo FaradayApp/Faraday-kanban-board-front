6af2743cfc18b0d5cf72074c090afc185b37a9c9
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 17:28:48 +0300

    wip: add board card

---
+++ src/widgets/admin/boards-management/board-card/BoardCard.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from 'react-i18next';
+
+import styles from './BoardCard.module.scss';
+import { TextArea, TrashIcon, Link, Heading } from '@/shared/ui-kit';
+import { type Board } from '@/enitities/admin';
+
+type BoardCardProps = {
+  board: Board;
+  onDelete?: () => void;
+};
+
+export const BoardCard = (props: BoardCardProps) => {
+  const { t } = useTranslation();
+  const { board, onDelete } = props;
+  const { uuid, title } = board;
+
+  const link = `tmp-site/${uuid}`;
+
+  return (
+    <article className={styles.boardCard}>
+      <header className={styles.boardCard__header}>
+        <Heading tag='h3' size='sm' children={title} />
+        <TrashIcon onClick={onDelete} className={styles.boardCard__icon} />
+      </header>
+
+      <TextArea placeholder={t('widgetsManagement.placeholders.note')} defaultValue={title} />
+
+      <footer className={styles.boardCard__footer}>
+        <Link withCopy href={link || ''} />
+      </footer>
+    </article>
+  );
+};
