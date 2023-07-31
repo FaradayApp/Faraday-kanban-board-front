5436620f4221b0911d9c83ce7490ee0acc415313
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 18:19:00 +0300

    feat: validate create board response

--- src/shared/api/admin/boards/create-board/create-board.ts
+++ src/shared/api/admin/boards/create-board/create-board.ts
@@ -2,8 +2,10 @@ import { CreateBoard } from '@/enitities/admin';
 import { request } from '@/shared/http';
 import { serialize } from '@/shared/lib/serialize';
 import { toDto } from './create-board.dto';
+import { toBoard, validateBoardDto } from '../board.dto';
 
-export function createBoard(data: CreateBoard) {
+export async function createBoard(data: CreateBoard) {
   const options = { body: serialize(toDto(data)) };
-  return request.post('board/', options);
+  const response = await request.post('board/', options).json();
+  return toBoard(validateBoardDto(response));
 }
