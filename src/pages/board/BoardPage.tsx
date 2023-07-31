e0561f4729865a68f78ca7fe20aa9c1dee0e321d
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 13:19:56 +0300

    feat: board page

--- src/pages/board/BoardPage.tsx
+++ src/pages/board/BoardPage.tsx
@@ -1,24 +1,34 @@
+import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import styles from './BoardPage.module.scss';
-import { boardStore } from '@/stores';
 import { PageContainer } from '@/shared/ui-kit';
+import { boardStore } from '@/stores/board/BoardStore';
 import { BoardPageHeader, TaskCard, TasksContainer, TasksSort } from '@/widgets/board';
 
 export const BoardPage = observer(() => {
+  useEffect(() => {
+    boardStore.init();
+  }, []);
+
   return (
     <PageContainer header={<BoardPageHeader />}>
       <div className={styles.boardPage__taskContainers}>
-        {boardStore.columns.map((column) => (
-          <TasksContainer
-            key={column.id}
-            title={column.status}
-            control={<TasksSort onSort={(sortType) => column.sortTasks(sortType)} />}>
-            {column.tasks.map((task) => (
-              <TaskCard key={task.data.id} task={task} />
-            ))}
-          </TasksContainer>
-        ))}
+        {boardStore.columns.map(
+          (columnStore) =>
+            !columnStore.isEmpty && (
+              <TasksContainer
+                key={columnStore.title}
+                title={columnStore.title}
+                control={
+                  <TasksSort onSort={columnStore.sort} selected={columnStore.options.sort} />
+                }>
+                {columnStore.tasks.map((task) => (
+                  <TaskCard key={task.id} {...task} />
+                ))}
+              </TasksContainer>
+            )
+        )}
       </div>
     </PageContainer>
   );
