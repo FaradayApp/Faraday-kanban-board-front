3850bb6353928e2fd81c45fec5c2dfcdb278ad49
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 13:19:30 +0300

    feat: user auth page

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,15 +1,18 @@
 import { observer } from 'mobx-react-lite';
 
 import styles from './AuthPage.module.scss';
-import { sessionStore } from '@/stores/Session';
 import { PageContainer } from '@/shared/ui-kit';
+import { sessionStore } from '@/stores/session/SessionStore';
+import { loginUser } from '@/features/auth';
 import { AuthForm } from '@/widgets/auth-form';
+
+const login = loginUser(sessionStore);
 
 export const AuthPage = observer(() => {
   return (
     <PageContainer>
       <div className={styles.formContainer}>
-        <AuthForm login={sessionStore.login.bind(sessionStore)} />
+        <AuthForm login={login} />
       </div>
     </PageContainer>
   );
