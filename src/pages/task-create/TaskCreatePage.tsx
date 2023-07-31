70af7c155c9af80bec903511139c4f6d74a96b00
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 13:21:48 +0300

    feat: task create page

--- src/pages/task-create/TaskCreatePage.tsx
+++ src/pages/task-create/TaskCreatePage.tsx
@@ -1,15 +1,26 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { useTranslation } from 'react-i18next';
 
 import { PageContainer } from '@/shared/ui-kit';
+import { type NewTask } from '@/enitities/task';
+import { boardStore } from '@/stores/board';
+import { addNewTask } from '@/features/tasks';
 import { TaskCreateForm, TaskPageHeader } from '@/widgets/task';
 
 export const TaskCreatePage = observer(() => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const addTask = useCallback(
+    (newTask: NewTask) => addNewTask(boardStore, navigate)(newTask),
+    [navigate]
+  );
 
   return (
     <PageContainer header={<TaskPageHeader title={t('taskEdit.titles.create')} />}>
-      <TaskCreateForm />
+      <TaskCreateForm addTask={addTask} />
     </PageContainer>
   );
 });
