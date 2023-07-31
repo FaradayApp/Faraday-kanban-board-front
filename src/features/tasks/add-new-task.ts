23defe8669ce1e82ea0cad5aab2105d97323f7f5
nokkup <kuprin.kirill.96@gmail.com>
2023-07-31 14:19:49 +0300

    feat: add new task feature

---
+++ src/features/tasks/add-new-task.ts
@@ -0,0 +1,12 @@
+import { NavigateFunction } from 'react-router-dom';
+
+import { NewTask } from '@/enitities/task';
+import { createNewTask } from '@/shared/api';
+import { BoardStore } from '@/stores/board/BoardStore';
+
+export function addNewTask(boardStore: BoardStore, navigate: NavigateFunction) {
+  return async function (newTask: NewTask) {
+    await createNewTask(boardStore.boardId, newTask);
+    navigate('/board/');
+  };
+}
