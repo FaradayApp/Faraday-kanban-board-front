1a08fed1e8a94718b0bd98223f2f8fbadd05d077
nokkup <kuprin.kirill.96@gmail.com>
2023-08-01 13:01:31 +0300

    feat: add navigate to edit page option

--- src/pages/task-info/TaskInfoPage.tsx
+++ src/pages/task-info/TaskInfoPage.tsx
@@ -1,9 +1,9 @@
 import { useCallback, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-import { PageContainer } from '@/shared/ui-kit';
+import { EditIcon, PageContainer } from '@/shared/ui-kit';
 import { type TaskInfo } from '@/enitities/task';
 import { boardStore } from '@/stores/board';
 import { taskInfoStore } from '@/stores/task-info';
@@ -13,6 +13,7 @@ import { TaskInfoForm, TaskPageHeader } from '@/widgets/task';
 export const TaskInfoPage = observer(() => {
   const { t } = useTranslation();
   const { id, boardId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (id && boardId) {
@@ -29,10 +30,20 @@ export const TaskInfoPage = observer(() => {
     [id]
   );
 
+  const openEditTaskPage = () => {
+    navigate('edit');
+  };
+
   const data = taskInfoStore.taskInfo.data || null;
 
   return (
-    <PageContainer header={<TaskPageHeader title={t('task.title')} />}>
+    <PageContainer
+      header={
+        <TaskPageHeader 
+          title={t('task.title')} 
+          options={<EditIcon onClick={openEditTaskPage} />} 
+        />
+      }>
       {data && <TaskInfoForm task={data} editTask={editTask} />}
     </PageContainer>
   );
