63bec7a8f94659fa4b18f89d8da4690ab9d08216
nokkup <kuprin.kirill.96@gmail.com>
2023-08-01 13:56:24 +0300

    feat: add created task in store

--- src/features/tasks/add-new-task.ts
+++ src/features/tasks/add-new-task.ts
@@ -6,7 +6,8 @@ import { BoardStore } from '@/stores/board/BoardStore';
 
 export function addNewTask(boardStore: BoardStore, navigate: NavigateFunction) {
   return async function (newTask: NewTask) {
-    await createNewTask(boardStore.boardId, newTask);
+    const createdTask = await createNewTask(boardStore.boardId, newTask);
+    boardStore.addNewTask(createdTask);
     navigate(`/board/${boardStore.boardId}/`);
   };
 }
