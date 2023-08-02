6d90f7e9f909d8cd53ebd2df45d2fe57b41032be
nokkup <kuprin.kirill.96@gmail.com>
2023-08-02 16:11:05 +0300

    feat: users public import

--- src/enitities/user/index.ts
+++ src/enitities/user/index.ts
@@ -1,3 +1,4 @@
+export { type User } from './user.types';
 export { UserShortCard } from './short-card';
 export { UsersMultiselect } from './users-multiselect';
 export { UserComment } from './comment';
