f79cc03e26c99e825cbf74707579a0360d77771b
nokkup <kuprin.kirill.96@gmail.com>
2023-08-03 18:55:30 +0300

    feat: edit and delete coments handlers

--- src/pages/task-info/TaskInfoPage.tsx
+++ src/pages/task-info/TaskInfoPage.tsx
@@ -6,22 +6,36 @@ import { useTranslation } from 'react-i18next';
 import { EditIcon, PageContainer } from '@/shared/ui-kit';
 import { boardStore } from '@/stores/board';
 import { taskInfoStore } from '@/stores/task-info';
-import { type EditTaskInfo, editTaskInfo } from '@/features/tasks';
+import {
+  type EditTaskInfo,
+  editTaskInfo,
+  deleteTaskComment,
+  editTaskComment,
+} from '@/features/tasks';
 import { TaskInfoForm, TaskPageHeader } from '@/widgets/task';
 
 export const TaskInfoPage = observer(() => {
   const { t } = useTranslation();
   const { id } = useParams();
   const navigate = useNavigate();
-  
+
   const editTask = useCallback(
     (data: EditTaskInfo) => {
       if (id) {
-        editTaskInfo(boardStore, taskInfoStore, id)(data);
+        const newData = { ...taskInfoStore.taskInfo.data, ...data };
+        editTaskInfo(boardStore, taskInfoStore, id)(newData);
       }
     },
     [id]
   );
+
+  const deleteComment = useCallback((commentId: number) => {
+    deleteTaskComment(boardStore, taskInfoStore)(commentId);
+  }, []);
+
+  const editComment = useCallback((commentId: number, message: string) => {
+    editTaskComment(boardStore, taskInfoStore)(commentId, message);
+  }, []);
 
   const openEditTaskPage = () => {
     navigate('edit');
@@ -34,7 +48,14 @@ export const TaskInfoPage = observer(() => {
       header={
         <TaskPageHeader title={t('task.title')} options={<EditIcon onClick={openEditTaskPage} />} />
       }>
-      {data && <TaskInfoForm task={data} editTask={editTask} />}
+      {data && (
+        <TaskInfoForm
+          task={data}
+          editTask={editTask}
+          deleteComment={deleteComment}
+          editComment={editComment}
+        />
+      )}
     </PageContainer>
   );
 });
