4abff5749e4e775d062ff00f6db6f78c7d2cf3e8
nokkup <kuprin.kirill.96@gmail.com>
2023-08-03 17:00:48 +0300

    feat: cursor pointer style

--- src/shared/ui-kit/icons/TrashIcon.tsx
+++ src/shared/ui-kit/icons/TrashIcon.tsx
@@ -1,7 +1,13 @@
 import { IconProps } from './types';
 
 export const TrashIcon = (props: IconProps) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width={18} height={18} fill='none' {...props}>
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={18}
+    height={18}
+    cursor='pointer'
+    fill='none'
+    {...props}>
     <path
       fill='#F25C5C'
       d='M14.25 3h-2.625l-.75-.75h-3.75l-.75.75H3.75v1.5h10.5M4.5 14.25a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5v-9h-9v9Z'
