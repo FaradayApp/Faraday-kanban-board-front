a81b0b6eaeb3f7d9b98f52d9825b98be37b58438
nokkup <kuprin.kirill.96@gmail.com>
2023-08-03 16:03:19 +0300

    feat: add CheckIcon cmp

---
+++ src/shared/ui-kit/icons/CheckIcon.tsx
@@ -0,0 +1,17 @@
+import { IconProps } from './types';
+
+export const CheckIcon = (props: IconProps) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={2.5}
+      stroke='currentColor'
+      width={18}
+      height={18}
+      {...props}>
+      <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 12.75l6 6 9-13.5' />
+    </svg>
+  );
+};
