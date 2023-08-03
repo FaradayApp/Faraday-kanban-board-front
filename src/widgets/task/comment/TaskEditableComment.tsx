ff6cc77ee8142d808928a3ffc1e7b242b3b2dca9
nokkup <kuprin.kirill.96@gmail.com>
2023-08-03 19:11:11 +0300

    feat: editable comments

--- src/widgets/task/comment/TaskEditableComment.tsx
+++ src/widgets/task/comment/TaskEditableComment.tsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import styles from './TaskEditableComment.module.scss';
 import { type TaskComment, TaskCommentCard, TaskCommentEditCard } from '@/enitities/task';
 import { CheckIcon, CloseIcon, EditIcon, TrashIcon } from '@/shared/ui-kit';
-import { useState } from 'react';
+import { TaskCommentSchema, taskCommentSchema } from './scheme';
 
 type TaskEditableCommentProps = {
   comment: TaskComment;
   withControls?: boolean;
-  deleteComment: (commentId: number) => void;
+  deleteComment: (commentId: number) => Promise<unknown>;
+  editComment: (commentId: number, message: string) => Promise<unknown>;
 };
 
 export const TaskEditableComment = (props: TaskEditableCommentProps) => {
-  const { comment, deleteComment } = props;
+  const { comment, deleteComment, editComment } = props;
   const [mode, setMode] = useState<'preview' | 'edit'>('preview');
+
+  const { handleSubmit, control } = useForm<TaskCommentSchema>({
+    resolver: zodResolver(taskCommentSchema),
+  });
+
+  const onSubmit: SubmitHandler<TaskCommentSchema> = async (data) => {
+    await editComment(comment.id, data.message);
+    setMode('preview');
+  };
 
   return mode === 'preview' ? (
     <TaskCommentCard
@@ -24,18 +38,28 @@ export const TaskEditableComment = (props: TaskEditableCommentProps) => {
       }
     />
   ) : (
-    <TaskCommentEditCard
-      key={comment.id}
-      user={comment.user}
-      message={comment.text}
-      maxLength={600}
-      onChange={() => ({})}
-      controls={
-        <>
-          <CheckIcon className={styles.taskEditableComment__saveButton} />
-          <CloseIcon onClick={() => setMode('preview')} />
-        </>
-      }
+    <Controller
+      name='message'
+      control={control}
+      defaultValue={comment.text}
+      render={({ field }) => (
+        <TaskCommentEditCard
+          key={comment.id}
+          user={comment.user}
+          message={field.value}
+          maxLength={600}
+          onChange={field.onChange}
+          controls={
+            <>
+              <CheckIcon
+                onClick={() => handleSubmit(onSubmit)()}
+                className={styles.taskEditableComment__saveButton}
+              />
+              <CloseIcon onClick={() => setMode('preview')} />
+            </>
+          }
+        />
+      )}
     />
   );
 };
