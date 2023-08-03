ea3cd715290a92ac51495a2eb178ff69a17a3e58
nokkup <kuprin.kirill.96@gmail.com>
2023-08-03 18:55:01 +0300

    feat: add edit task comment feature

---
+++ src/features/tasks/edit-comment.ts
@@ -0,0 +1,14 @@
+import { editTaskComment as editTaskCommentApi, getTaskInfo } from '@/shared/api';
+import { BoardStore, TaskInfoStore } from '@/stores';
+
+export function editTaskComment(boardStore: BoardStore, taskInfoStore: TaskInfoStore) {
+  return async function (commentId: number, message: string) {
+    if (boardStore.boardId && taskInfoStore.taskId && commentId) {
+      await editTaskCommentApi(boardStore.boardId, taskInfoStore.taskId, commentId, message);
+
+      const { task, taskInfo } = await getTaskInfo(boardStore.boardId, taskInfoStore.taskId);
+      boardStore.updateTask(task);
+      taskInfoStore.updateTaskInfo(taskInfo);
+    }
+  };
+}
