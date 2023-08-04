c413550a71f30990fa66453fac188684a6544d4a
nokkup <kuprin.kirill.96@gmail.com>
2023-08-04 12:29:30 +0300

    feat: providers structure

---
+++ src/providers/data/index.ts
@@ -0,0 +1,3 @@
+export { BoardProvider } from './board.provider';
+export { TaskInfoProvider } from './task-info.provider';
+export { ProfileProvider } from './profile.provider';
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,5 +1,3 @@
-export { I18nProvider } from './i18n.provider';
-export { TaskInfoProvider } from './task-info.provider';
-export { BoardProvider } from './board.provider';
-export { RouterProvider } from './router.provider';
-export { SwrProvider } from './swr.provider';
+export { TaskInfoProvider, BoardProvider } from './data';
+export { I18nProvider } from './i18n';
+export { RouterProvider } from './router';
