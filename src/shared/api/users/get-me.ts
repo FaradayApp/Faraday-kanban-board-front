57d87875b22aac02380fe03a4ee609ac9c4f178b
nokkup <kuprin.kirill.96@gmail.com>
2023-08-04 12:49:51 +0300

    fix: get response data

--- src/shared/api/users/get-me.ts
+++ src/shared/api/users/get-me.ts
@@ -2,6 +2,6 @@ import { request } from '@/shared/http';
 import { toUser, validateUserDto } from './dtos';
 
 export async function getMe() {
-  const response = await request.get('user/');
+  const response = await request.get('user/').json();
   return toUser(validateUserDto(response));
 }
