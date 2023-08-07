78751fd689ece31039a6a998ba84cf890eee51a6
nokkup <kuprin.kirill.96@gmail.com>
2023-08-07 15:52:46 +0300

    feat: show loader feature

--- src/shared/ui-kit/containers/page-container/PageContainer.tsx
+++ src/shared/ui-kit/containers/page-container/PageContainer.tsx
@@ -1,18 +1,26 @@
 import { PropsWithChildren, ReactNode } from 'react';
 
 import styles from './PageContainer.module.scss';
+import { Loader } from '../..';
 
 type PageContainerProps = PropsWithChildren<{
   header?: ReactNode;
+  loading?: boolean;
 }>;
 
 export const PageContainer = (props: PageContainerProps) => {
-  const { children, header } = props;
+  const { children, header, loading } = props;
 
   return (
-    <main className={styles.container} >
+    <main className={styles.container}>
       {header && <header className={styles.container__header}>{header}</header>}
-      <div className={styles.container__body}>{children}</div>
+      {loading ? (
+        <div className={styles.container__loader}>
+          <Loader />
+        </div>
+      ) : (
+        <div className={styles.container__body}>{children}</div>
+      )}
     </main>
   );
 };
