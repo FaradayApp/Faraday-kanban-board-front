a11505b76b007cefebe0040d904c71ea5ecaf9a0
nokkup <kuprin.kirill.96@gmail.com>
2023-08-17 12:35:55 +0300

    feat: increrase container height

--- src/widgets/board/tasks-container/TasksContainer.tsx
+++ src/widgets/board/tasks-container/TasksContainer.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, ReactNode } from 'react';
 
 import styles from './TasksContainer.module.scss';
 import { Heading } from '@/shared/ui-kit';
+import { use100vh } from 'react-div-100vh';
 
 type TasksContainerProps = PropsWithChildren<{
   title: string;
@@ -10,9 +11,10 @@ type TasksContainerProps = PropsWithChildren<{
 
 export const TasksContainer = (props: TasksContainerProps) => {
   const { title, control, children } = props;
+  const vh = use100vh();
 
   return (
-    <article className={styles.tasksContainer}>
+    <article className={styles.tasksContainer} style={{ maxHeight: vh ? vh - 120 : '80vh' }}>
       <header className={styles.tasksContainer__header}>
         <Heading tag='h3' size='xsm' weight='bold' children={title} />
         {control}
