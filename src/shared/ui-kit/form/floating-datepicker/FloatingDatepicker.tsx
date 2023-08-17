745b8368b522fde5d11c1141b08fc8c95ec23c1c
nokkup <kuprin.kirill.96@gmail.com>
2023-08-17 14:00:18 +0300

    feat: remove inner ref and show picker by container click

--- src/shared/ui-kit/form/floating-datepicker/FloatingDatepicker.tsx
+++ src/shared/ui-kit/form/floating-datepicker/FloatingDatepicker.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, forwardRef, useId } from 'react';
+import { ChangeEvent, useId, useRef } from 'react';
 import clsx from 'clsx';
 
 import styles from './FloatingDatepicker.module.scss';
@@ -13,50 +13,53 @@ type FloatingDetepickerProps = {
   errorMessage?: string;
 };
 
-export const FloatingDetepicker = forwardRef<HTMLInputElement, FloatingDetepickerProps>(
-  (props, ref) => {
-    const id = useId();
-    const { label, value, errorMessage, isInvalid, onChange } = props;
+export const FloatingDetepicker = (props: FloatingDetepickerProps) => {
+  const id = useId();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const { label, value, errorMessage, isInvalid, onChange } = props;
 
-    const containerClasses = clsx(styles.floatingDatepicker, {
-      [styles.floatingDatepicker_invalid]: isInvalid || errorMessage,
-      [styles.floatingDatepicker_unfilled]: !value,
-      [styles.floatingDatepicker_filled]: !!value,
-    });
+  const containerClasses = clsx(styles.floatingDatepicker, {
+    [styles.floatingDatepicker_invalid]: isInvalid || errorMessage,
+    [styles.floatingDatepicker_unfilled]: !value,
+    [styles.floatingDatepicker_filled]: !!value,
+  });
 
-    const onDateChange = (event: ChangeEvent<HTMLInputElement>) => {
-      if (event.target.value) {
-        onChange(dayjs(event.target.value));
-      }
-    };
+  const onDateChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value) {
+      onChange(dayjs(event.target.value));
+    }
+  };
 
-    return (
-      <div className={containerClasses}>
-        {value && (
-          <Text tag='p' size='md' className={styles.date}>
-            {value.format('DD.MM.YYYY')}
-          </Text>
-        )}
+  const openDatePicker = () => {
+    inputRef.current?.showPicker();
+  };
 
-        <input
-          type='date'
-          id={id}
-          ref={ref}
-          placeholder={label}
-          className={styles.datepicker}
-          aria-invalid={!!errorMessage || isInvalid}
-          aria-errormessage={errorMessage}
-          onChange={onDateChange}
-        />
+  return (
+    <label htmlFor={id} className={containerClasses} onClick={openDatePicker}>
+      {value && (
+        <Text tag='p' size='md' className={styles.date}>
+          {value.format('DD.MM.YYYY')}
+        </Text>
+      )}
 
-        <label htmlFor={id} className={styles.label}>
-          {label}
-        </label>
+      <input
+        type='date'
+        id={id}
+        ref={inputRef}
+        placeholder={label}
+        className={styles.datepicker}
+        aria-invalid={!!errorMessage || isInvalid}
+        aria-errormessage={errorMessage}
+        onChange={onDateChange}
+      />
 
-        <div className={styles.floatingDatepicker__icon}>
-          <CalendarIcon />
-        </div>
+      <div className={styles.label}>
+        {label}
       </div>
-    );
-  }
-);
+
+      <div className={styles.floatingDatepicker__icon}>
+        <CalendarIcon />
+      </div>
+    </label>
+  );
+};
