f6bf09e13b82a8dd6e24a6f57e30fa6050dff4cc
nokkup <kuprin.kirill.96@gmail.com>
2023-08-17 15:30:38 +0300

    feat: notifications

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
+import 'react-toastify/dist/ReactToastify.css';
 import '@/shared/styles/styles.scss';
 import '@/shared/styles/theme.scss';
-import 'react-toastify/dist/ReactToastify.css';
 import { I18nProvider, RouterProvider } from '@/providers';
 import { WideContainer } from '@/shared/ui-kit';
 import { ToastContainer } from 'react-toastify';
