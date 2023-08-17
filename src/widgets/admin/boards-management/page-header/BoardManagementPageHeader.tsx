967e00e474b97a0f111f1f1b75c8acf7dda2cea8
nokkup <kuprin.kirill.96@gmail.com>
2023-08-17 11:50:09 +0300

    feat: logout confirm modal

--- src/widgets/admin/boards-management/page-header/BoardManagementPageHeader.tsx
+++ src/widgets/admin/boards-management/page-header/BoardManagementPageHeader.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next';
 
 import styles from './BoardManagementPageHeader.module.scss';
 import { sessionStore } from '@/stores/session/SessionStore';
-import { Heading, LogoutIcon } from '@/shared/ui-kit';
+import { ConfirmModal, Heading, LogoutIcon } from '@/shared/ui-kit';
 
 export const BoardManagementPageHeader = () => {
   const { t } = useTranslation();
@@ -10,10 +10,13 @@ export const BoardManagementPageHeader = () => {
   return (
     <div className={styles.container}>
       <Heading size='lg'>{t('widgetsManagement.title')}</Heading>
-      <LogoutIcon 
-        onClick={() => sessionStore.clearSession()} 
-        className={styles.container__icon}
-      />
+      <ConfirmModal
+        title={t('exitModal.title')}
+        approveTitle={t('exitModal.approve')}
+        cancelTitle={t('exitModal.cancel')}
+        onApprove={() => sessionStore.clearSession()}>
+        <LogoutIcon className={styles.container__icon} />
+      </ConfirmModal>
     </div>
   );
 };
